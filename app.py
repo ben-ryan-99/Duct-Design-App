1e7683edcbbc1950753937abb47e0fc5cb542f6b
Ben Ryan <benjaminryan1099@gmail.com>
2026-06-04 14:53:39 -0400

    add item list

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from ductcalc.models import DuctSegment, Fitting, Path
 from ductcalc.system import calculate_path_pressure_drop
 from ductcalc.fitting_db import FITTINGS
 
+if "path_items" not in st.session_state:
+    st.session_state["path_items"] = []
 
 st.title("Duct Pressure Drop Calculator")
 
@@ -22,6 +24,57 @@ selected_fitting_id = st.selectbox(
     format_func=lambda x: FITTINGS[x]["name"]
 )
 
+
+if st.button("Add Duct"):
+    st.session_state["path_items"].append(
+        {
+            "type": "duct",
+            "length_ft": length_ft,
+            "diameter_in": diameter_in,
+            "airflow_cfm": airflow_cfm,
+        }
+    )
+
+if st.button("Add Fitting"):
+    st.session_state["path_items"].append(
+        {
+            "type": "fitting",
+            "fitting_type": selected_fitting_id,
+            "diameter_in": diameter_in,
+            "airflow_cfm": airflow_cfm,
+        }
+    )
+
+if st.button("Clear Path"):
+    st.session_state["path_items"] = []
+    st.rerun()
+
+
+
+
+# Display the item list
+st.subheader("Current Path")
+
+for i, item in enumerate(st.session_state["path_items"]):
+
+    if item["type"] == "duct":
+        st.write(
+            f"{i+1}. Duct - "
+            f"{item['length_ft']} ft, "
+            f"{item['diameter_in']} in"
+        )
+
+    elif item["type"] == "fitting":
+        st.write(
+            f"{i+1}. Fitting - "
+            f"{item['fitting_type']}"
+        )
+
+
+
+
+
+######  Calculation
 if st.button("Calculate"):
     duct = DuctSegment(
         length_ft=length_ft,
